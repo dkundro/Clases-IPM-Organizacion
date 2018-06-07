50af0f995a7cff61c935f669a29ca8ada7208195
Daniel <dkundro@dc.uba.ar>
2018-06-06 21:43:36 -0300

    actualizacion: ejercicios fork

--- Fork/Ej1.cpp
+++ Fork/Ej1.cpp
@@ -6,13 +6,9 @@
 #include <sys/wait.h>
 using namespace std;
 
+
 int main ()
 {
-	//Consigna: crear tres procesos hijos. Cada proceso hijo debe imprimir su PID y luego finalizar. El proceso padre, luego de crear a sus hijos, imprimira su PID, esperara que sus tres hijos hayan terminado y luego finalizara.
 
-
-
-
-	return 0;
 }
 
--- Fork/Ej3.cpp
+++ Fork/Ej3.cpp
@@ -9,15 +9,13 @@ using namespace std;
 int main ()
 {
 	//Consigna: El proceso principal crea dos procesos hijos:
-	//	Productor: Este proceso escribira A, B , C o D aleatoriamente en un archivo de 
-	//		texto cada 3 segundos (usar spleep(3);). Repetira este comportamiento en 
+	//	Productor: Este proceso escribira A, B , C o D aleatoriamente en un pipe
+	//		cada 3 segundos (usar spleep(3);). Repetira este comportamiento en 
 	//		un bucle infinito (while(true){})
 	//	Consumidor: Este proceso estara dentro de un bucle infinito chequeando 
-	//		constantemente si el proceso productor escribio algo en el archivo.
-	//		Segun la letra que haya escrito el productor, este proceso imprimira algo
-	//		por pantalla. Finalmente, borrara el contenido del archivo y volvera al 
-	//		inicio del loop.
-
+	//		constantemente si el proceso productor escribio algo en el pipe.
+	//		Segun la letra que haya escrito el productor, este proceso imprimira algo 
+	//		por pantalla. Finalmente, Volvera a esperar una letra en el pipe 
 
 	return 0;
 }
